fe4ae708c4ba356a445c5c1032dc2b997a9055c6
Milan Vojnovic <milanvojnovic@gmail.com>
2016-12-01 15:31:19 +0100

    check if loans are displayed

--- src/main/java/PageObjects/MarketPlacePage.java
+++ src/main/java/PageObjects/MarketPlacePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 
+import java.util.List;
+
 public class MarketPlacePage extends LoadableComponent<MarketPlacePage> {
 
     WebDriver driver;
@@ -14,28 +16,31 @@ public class MarketPlacePage extends LoadableComponent<MarketPlacePage> {
     @FindBy(xpath = ".//div[@class='main']//h1")
     private WebElement pageHeader;
 
+    @FindBy(className = "item")
+    public List<WebElement> loanItems;
+
 
     public MarketPlacePage(WebDriver webDriver) {
         driver = webDriver;
         PageFactory.initElements(driver, this);
-
     }
 
     @Override
     protected void load() {
         driver.get(pageUrl);
-
     }
 
     @Override
     protected void isLoaded() throws Error {
-        //new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOf(pageHeader));
         String actualUrl = driver.getCurrentUrl();
         assert (actualUrl.equals(pageUrl));
-
     }
 
     public String getPageHeaderText() {
         return pageHeader.getText();
+    }
+
+    public int loanItemsOnPage() {
+        return loanItems.size();
     }
 }
--- src/test/java/MarketPlaceSmokeTest.java
+++ src/test/java/MarketPlaceSmokeTest.java
@@ -7,25 +7,35 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 
 public class MarketPlaceSmokeTest {
 
     static WebDriver driver;
 
     @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
         driver = new FirefoxDriver();
     }
 
     @Test
-    public void marketplacePageSmokeTest() {
+    public void titleHeaderIsDisplayed() {
+
+        String expectedText = "Tržiště";
 
         MarketPlacePage page = new MarketPlacePage(driver).get();
-        assertThat(page.getPageHeaderText(), is("Tržiště"));
+        assertThat(String.format("Could not find text %s in page header", expectedText), page.getPageHeaderText(), is(expectedText));
+    }
+
+    @Test
+    public void loansDisplayed() {
+
+        MarketPlacePage page = new MarketPlacePage(driver).get();
+        assertThat("There is not a single loan on page.", page.loanItemsOnPage(), not(0));
     }
 
     @AfterClass
-    public static void tearDown(){
+    public static void tearDown() {
         driver.quit();
     }
 }
