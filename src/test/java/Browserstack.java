fa4f6cee8d3677aef085d6698aff6ed792a4a15f
Milan Vojnovic <milan.vojnovic@zonky.cz>
2017-01-09 22:02:33 +0100

    demo test

---
+++ src/test/java/Browserstack.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class Browserstack {
+
+    public static final String USERNAME = "milanvojnovic";
+    public static final String AUTOMATE_KEY = "qK8r62hczAFVpFEesPoh";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    public static void main(String[] args) throws Exception {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browser", "Edge");
+        caps.setCapability("browser_version", "14.0");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1280x1024");
+
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("BrowserStack");
+        element.submit();
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+    }
+}
